31594f2e02d2d1f8683fc35c22d5aeba9fef748c
JADENgygo <29948746+JADENgygo@users.noreply.github.com>
2025-09-26 20:33:44 +0900

    Added a comment to init0.sql on dev branch

--- init0.sql
+++ init0.sql
@@ -5,3 +5,6 @@ alter system set log_truncate_on_rotation = 'on';
 alter system set log_min_duration_statement = 1000;
 alter system set log_lock_waits = 'on';
 select pg_reload_conf();
+
+-- Added a comment to demonstrate a change
+-- This is a new comment added on dev branch
